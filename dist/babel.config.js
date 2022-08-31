63f6f1a9e897f5f1a92e27f41474bd14c946b89f
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-08-31 16:26:07 +0800

    fix user service experimental modules issue

---
+++ dist/babel.config.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = (api) => {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module:react-native-dotenv',
+                {
+                    envName: 'APP_ENV',
+                    moduleName: '@env',
+                    path: '.env',
+                    blocklist: null,
+                    allowlist: null,
+                    blacklist: null,
+                    whitelist: null,
+                    safe: false,
+                    allowUndefined: true,
+                    verbose: false,
+                },
+            ],
+            [
+                'module-resolver',
+                {
+                    root: ['./dist/'],
+                    extensions: [
+                        '.js',
+                        '.jsx',
+                        '.ts',
+                        '.tsx',
+                        '.json',
+                    ],
+                },
+            ],
+        ],
+    };
+};
