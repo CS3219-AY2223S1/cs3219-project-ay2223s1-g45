e73e7ac0c330a3ee0096d1c2452177030502a914
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-08-31 16:51:56 +0800

    format babel config

--- dist/frontend/src/App.js
+++ dist/frontend/src/App.js
@@ -3,11 +3,29 @@ import SignupPage from './components/SignupPage';
 import { Box } from '@mui/material';
 import React from 'react';
 function App() {
-    return (React.createElement("div", { className: "App" },
-        React.createElement(Box, { display: 'flex', flexDirection: 'column', padding: '4rem' },
-            React.createElement(Router, null,
-                React.createElement(Routes, null,
-                    React.createElement(Route /*exact*/, { path: "/", element: React.createElement(Navigate, { replace: true, to: "/signup" }) }),
-                    React.createElement(Route, { path: "/signup", element: React.createElement(SignupPage, null) }))))));
+  return React.createElement(
+    'div',
+    { className: 'App' },
+    React.createElement(
+      Box,
+      { display: 'flex', flexDirection: 'column', padding: '4rem' },
+      React.createElement(
+        Router,
+        null,
+        React.createElement(
+          Routes,
+          null,
+          React.createElement(Route /*exact*/, {
+            path: '/',
+            element: React.createElement(Navigate, { replace: true, to: '/signup' })
+          }),
+          React.createElement(Route, {
+            path: '/signup',
+            element: React.createElement(SignupPage, null)
+          })
+        )
+      )
+    )
+  );
 }
 export default App;
--- dist/user-service/model/repository.js
+++ dist/user-service/model/repository.js
@@ -1,12 +1,34 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
+  };
 import mongoose from 'mongoose';
 import UserModel from './user-model';
 import 'dotenv/config';
@@ -16,7 +38,7 @@ const db = mongoose.connection;
 // eslint-disable-next-line no-console
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 export default function createUser(params) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new UserModel(params);
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    return new UserModel(params);
+  });
 }
