076bae07b64b4c889d62341754c4f4082c98f650
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-03 19:16:58 +0800

    separate packages

--- user-service/dist/index.js
+++ user-service/dist/index.js
@@ -10,9 +10,9 @@ const router = express.Router();
 // Controller will contain all the User-defined Routes
 router.get('/', (_, res) => res.send('Hello World from user-service'));
 router.post('/', createUser);
-app.use('/api/user', router).all(((_, res) => {
-    res.setHeader('content-type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-}));
+app.use('/api/user', router).all((_, res) => {
+  res.setHeader('content-type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+});
 // eslint-disable-next-line no-console
 app.listen(8000, () => console.log('user-service listening on port 8000'));
---
+++ user-service/dist/model/repository.js
@@ -0,0 +1,44 @@
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+import mongoose from 'mongoose';
+import UserModel from './user-model';
+import 'dotenv/config';
+const mongoDB = process.env.ENV === 'PROD' ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+export default function createUser(params) {
+  return __awaiter(this, void 0, void 0, function* () {
+    return new UserModel(params);
+  });
+}
