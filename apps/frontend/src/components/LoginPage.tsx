873f8f42a9f1be5b9896f00fe93d48325b917eac
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-05 16:02:02 +0800

    add Login and Settings pages

--- apps/frontend/src/components/LoginPage.tsx
+++ apps/frontend/src/components/LoginPage.tsx
@@ -1,102 +1,101 @@
 import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-    TextField,
-    Typography
-  } from '@mui/material';
-  import { useState } from 'react';
-  import axios from 'axios';
-  import { URL_USER_SVC } from '../configs';
-  import { STATUS_CODE_CONFLICT, STATUS_CODE_OK } from '../constants';
-  import { Link } from 'react-router-dom';
-  
-  function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [dialogTitle, setDialogTitle] = useState('');
-    const [dialogMsg, setDialogMsg] = useState('');
-    const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-  
-    const handleLogin = async () => {
-      setIsLoginSuccess(false);
-      const res = await axios.post(`${URL_USER_SVC}/login`, { username, password }).catch((err) => {
-        if (err.response.status === STATUS_CODE_CONFLICT) {
-          setErrorDialog('This username already exists');
-        } else {
-          setErrorDialog('Please try again later');
-        }
-      });
-      if (res && res.status === STATUS_CODE_OK) {
-        setSuccessDialog('User successfully logged in');
-        setIsLoginSuccess(true);
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  TextField,
+  Typography
+} from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+import { URL_USER_SVC } from '../configs';
+import { STATUS_CODE_CONFLICT, STATUS_CODE_OK } from '../constants';
+import { Link } from 'react-router-dom';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState('');
+  const [dialogMsg, setDialogMsg] = useState('');
+  const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+
+  const handleLogin = async () => {
+    setIsLoginSuccess(false);
+    const res = await axios.post(`${URL_USER_SVC}/login`, { username, password }).catch((err) => {
+      if (err.response.status === STATUS_CODE_CONFLICT) {
+        setErrorDialog('This username already exists');
+      } else {
+        setErrorDialog('Please try again later');
       }
-    };
-  
-    const closeDialog = () => setIsDialogOpen(false);
-  
-    const setSuccessDialog = (msg: any) => {
-      setIsDialogOpen(true);
-      setDialogTitle('Success');
-      setDialogMsg(msg);
-    };
-  
-    const setErrorDialog = (msg: any) => {
-      setIsDialogOpen(true);
-      setDialogTitle('Error');
-      setDialogMsg(msg);
-    };
-  
-    return (
-      <Box display={'flex'} flexDirection={'column'} width={'30%'}>
-        <Typography variant={'h3'} marginBottom={'2rem'}>
+    });
+    if (res && res.status === STATUS_CODE_OK) {
+      setSuccessDialog('User successfully logged in');
+      setIsLoginSuccess(true);
+    }
+  };
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const setSuccessDialog = (msg: any) => {
+    setIsDialogOpen(true);
+    setDialogTitle('Success');
+    setDialogMsg(msg);
+  };
+
+  const setErrorDialog = (msg: any) => {
+    setIsDialogOpen(true);
+    setDialogTitle('Error');
+    setDialogMsg(msg);
+  };
+
+  return (
+    <Box display={'flex'} flexDirection={'column'} width={'30%'}>
+      <Typography variant={'h3'} marginBottom={'2rem'}>
+        Log In
+      </Typography>
+      <TextField
+        label="Username"
+        variant="standard"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        sx={{ marginBottom: '1rem' }}
+        autoFocus
+      />
+      <TextField
+        label="Password"
+        variant="standard"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        sx={{ marginBottom: '2rem' }}
+      />
+      <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
+        <Button variant={'outlined'} onClick={handleLogin}>
           Log In
-        </Typography>
-        <TextField
-          label="Username"
-          variant="standard"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          sx={{ marginBottom: '1rem' }}
-          autoFocus
-        />
-        <TextField
-          label="Password"
-          variant="standard"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          sx={{ marginBottom: '2rem' }}
-        />
-        <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
-          <Button variant={'outlined'} onClick={handleLogin}>
-            Log In
-          </Button>
-        </Box>
-  
-        <Dialog open={isDialogOpen} onClose={closeDialog}>
-          <DialogTitle>{dialogTitle}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>{dialogMsg}</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            {isLoginSuccess ? (
-              <Button component={Link} to="/settings">
-                Log in
-              </Button>
-            ) : (
-              <Button onClick={closeDialog}>Done</Button>
-            )}
-          </DialogActions>
-        </Dialog>
+        </Button>
       </Box>
-    );
-  }
-  
+
+      <Dialog open={isDialogOpen} onClose={closeDialog}>
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{dialogMsg}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          {isLoginSuccess ? (
+            <Button component={Link} to="/settings">
+              Log in
+            </Button>
+          ) : (
+            <Button onClick={closeDialog}>Done</Button>
+          )}
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+}
+
 export default LoginPage;
-  
--- apps/frontend/src/components/SettingsPage.tsx
+++ apps/frontend/src/components/SettingsPage.tsx
@@ -1,80 +1,77 @@
 import {
-    Box,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
-  } from '@mui/material';
-  import { useState } from 'react';
-  import axios from 'axios';
-  import { URL_USER_SVC } from '../configs';
-  import { STATUS_CODE_CONFLICT, STATUS_CODE_OK } from '../constants';
-  import { useNavigate } from 'react-router-dom';
-  
-  function SettingsPage() {
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [dialogTitle, setDialogTitle] = useState('');
-    const [dialogMsg, setDialogMsg] = useState('');
-    const [isLogoutSuccess, setIsLogoutSuccess] = useState(false);
-    const navigate = useNavigate();
-  
-    const handleLogout = async () => {
-      setIsLogoutSuccess(false);
-      const res = await axios.post(`${URL_USER_SVC}/Logout`).catch((err) => {
-        if (err.response.status === STATUS_CODE_CONFLICT) {
-          setErrorDialog('This username already exists');
-        } else {
-          setErrorDialog('Please try again later');
-        }
-      });
-      if (res && res.status === STATUS_CODE_OK) {
-        setSuccessDialog('User successfully logged out');
-        setIsLogoutSuccess(true);
-      }
-    };
-  
-    const closeDialog = () => setIsDialogOpen(false);
-  
-    const setSuccessDialog = (msg: any) => {
-      setIsDialogOpen(true);
-      setDialogTitle('Success');
-      setDialogMsg(msg);
-    };
-  
-    const setErrorDialog = (msg: any) => {
-      setIsDialogOpen(true);
-      setDialogTitle('Error');
-      setDialogMsg(msg);
-    };
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from '@mui/material';
+import { useState } from 'react';
+import axios from 'axios';
+import { URL_USER_SVC } from '../configs';
+import { STATUS_CODE_CONFLICT, STATUS_CODE_OK } from '../constants';
+import { useNavigate } from 'react-router-dom';
 
-    const handleCloseDialog = () => {
-        isLogoutSuccess ? navigate('../login') : navigate('../settings');
+function SettingsPage() {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [dialogTitle, setDialogTitle] = useState('');
+  const [dialogMsg, setDialogMsg] = useState('');
+  const [isLogoutSuccess, setIsLogoutSuccess] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    setIsLogoutSuccess(false);
+    const res = await axios.post(`${URL_USER_SVC}/logout`).catch((err) => {
+      if (err.response.status === STATUS_CODE_CONFLICT) {
+        setErrorDialog('This username already exists');
+      } else {
+        setErrorDialog('Please try again later');
+      }
+    });
+    if (res && res.status === STATUS_CODE_OK) {
+      setSuccessDialog('User successfully logged out');
+      setIsLogoutSuccess(true);
     }
-  
-    return (
-      <Box display={'flex'} flexDirection={'column'} width={'30%'}>
-        <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
-          <Button variant={'outlined'} onClick={handleLogout}>
-            Log Out
-          </Button>
-        </Box>
-  
-        <Dialog open={isDialogOpen} onClose={closeDialog}>
-          <DialogTitle>{dialogTitle}</DialogTitle>
-          <DialogContent>
-            <DialogContentText>{dialogMsg}</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={() => handleCloseDialog()}>
-                Close
-            </Button>
-          </DialogActions>
-        </Dialog>
+  };
+
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const setSuccessDialog = (msg: any) => {
+    setIsDialogOpen(true);
+    setDialogTitle('Success');
+    setDialogMsg(msg);
+  };
+
+  const setErrorDialog = (msg: any) => {
+    setIsDialogOpen(true);
+    setDialogTitle('Error');
+    setDialogMsg(msg);
+  };
+
+  const handleCloseDialog = () => {
+    isLogoutSuccess ? navigate('../login') : navigate('../settings');
+  };
+
+  return (
+    <Box display={'flex'} flexDirection={'column'} width={'30%'}>
+      <Box display={'flex'} flexDirection={'row'} justifyContent={'flex-end'}>
+        <Button variant={'outlined'} onClick={handleLogout}>
+          Log Out
+        </Button>
       </Box>
-    );
-  }
-  
+
+      <Dialog open={isDialogOpen} onClose={closeDialog}>
+        <DialogTitle>{dialogTitle}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{dialogMsg}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => handleCloseDialog()}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+}
+
 export default SettingsPage;
-  
