1209ebd690958034a1559dc277c65c9240254e10
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-13 15:08:22 +0800

    add auth and unauth routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -7,7 +7,50 @@ import LobbyPage from './components/LobbyPage';
 import { Box } from '@mui/material';
 import NavigationBar from './components/NavigationBar';
 import { colorScheme } from './constants';
-import AuthContext from './context/AuthContext';
+import AuthContext, { useAuth } from './context/AuthContext';
+
+function AuthenticatedRoutes() {
+  return (
+    <Routes>
+      <Route /*exact*/ path="/" element={<Navigate replace to="/settings" />}></Route>
+      <Route path="/settings" element={<SettingsPage />} />
+      <Route path="/difficulty-select" element={<DifficultySelectPage />} />
+      <Route path="/lobby" element={<LobbyPage />} />
+      <Route path="*" element={<Navigate replace to="/settings"/>} />
+    </Routes>
+  );
+}
+
+function UnauthenticatedRoutes() {
+  return (
+    <Routes>
+      <Route /*exact*/ path="/" element={<Navigate replace to="/login" />}></Route>
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<Navigate replace to="/login"/>} />
+    </Routes>
+  );
+}
+
+function RouterContainer() {
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <Router>
+      <NavigationBar gridRowStart={1} />
+      <Box
+        display={'flex'}
+        flexDirection={'row'}
+        padding={'4rem'}
+        alignItems={'center'}
+        style={{
+          gridRowStart: '2'
+        }}>
+        {isAuthenticated ? <AuthenticatedRoutes /> : <UnauthenticatedRoutes />}
+      </Box>
+    </Router>
+  );
+}
 
 function App() {
   return (
@@ -19,29 +62,8 @@ function App() {
           gridTemplateRows: 'repeat(3, 1fr)',
           maxHeight: '100vh',
           background: `linear-gradient(90deg, ${colorScheme.primary}, ${colorScheme.secondary})`
-        }}
-      >
-        <Router>
-          <NavigationBar gridRowStart={1} />
-          <Box
-            display={'flex'}
-            flexDirection={'row'}
-            padding={'4rem'}
-            alignItems={'center'}
-            style={{
-              gridRowStart: '2'
-            }}
-          >
-            <Routes>
-              <Route /*exact*/ path="/" element={<Navigate replace to="/login" />}></Route>
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/settings" element={<SettingsPage />} />
-              <Route path="/difficulty-select" element={<DifficultySelectPage />} />
-              <Route path="/lobby" element={<LobbyPage />} />
-            </Routes>
-          </Box>
-        </Router>
+        }}>
+        <RouterContainer />
       </div>
     </AuthContext>
   );
