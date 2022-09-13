c1e6011f1a632cda0fbd441de6f5849532e6762d
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-13 13:43:41 +0800

    change max parallel projects to 4

--- apps/frontend/src/components/LoginPage.tsx
+++ apps/frontend/src/components/LoginPage.tsx
@@ -80,7 +80,8 @@ function LoginPage() {
       fontFamily={'Arimo'}
       borderRadius={'10px'}
       padding={'5%'}
-      style={{ backgroundColor: 'white' }}>
+      style={{ backgroundColor: 'white' }}
+    >
       <Box display={'flex'} width={'50%'}>
         <Logo />
       </Box>
@@ -137,7 +138,8 @@ function LoginPage() {
               borderColor: 'white',
               background: 'linear-gradient(90deg, #AC44B0, #EF429A)'
             }}
-            fullWidth>
+            fullWidth
+          >
             Log In
           </Button>
         </Box>
