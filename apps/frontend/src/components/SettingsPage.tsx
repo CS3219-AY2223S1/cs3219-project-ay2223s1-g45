f21e72b0b91e1334c2adaa4b5191675ba841421c
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-09-14 17:33:56 +0800

    refactor settings page (work in progress)

--- apps/frontend/src/components/SettingsPage.tsx
+++ apps/frontend/src/components/SettingsPage.tsx
@@ -1,244 +1,90 @@
-import {
-  Box,
-  Button,
-  Dialog,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField
-} from '@mui/material';
-import { useState } from 'react';
+import { Box, Button, Typography } from '@mui/material';
+import { Logo } from './Logo';
 import axios from 'axios';
-import { URL_USER_SVC } from '../configs';
-import {
-  STATUS_CODE_FORBIDDEN,
-  STATUS_CODE_INTERNAL_SERVER_ERROR,
-  STATUS_CODE_OK,
-  STATUS_CODE_UNAUTHORISED
-} from '../constants';
 import { useNavigate } from 'react-router-dom';
-import { Typography } from '@mui/material';
 import { useAuth } from '../context/AuthContext';
+import { URL_USER_SVC } from '../configs';
+import { STATUS_CODE_INTERNAL_SERVER_ERROR, STATUS_CODE_OK } from '../constants';
 
-function SettingsPage() {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [isDialogButtonEnabled, setIsDialogButtonEnabled] = useState(false);
-  const [isDialogInputEnabled, setIsDialogInputEnabled] = useState(false);
-  const [dialogButtonMessage, setDialogButtonMessage] = useState('');
-  const [buttonMode, setButtonMode] = useState('');
-  const [dialogTitle, setDialogTitle] = useState('');
-  const [dialogMsg, setDialogMsg] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [newPasswordErrorState, setNewPasswordErrorState] = useState(false);
-  const navigate = useNavigate();
+export default function SettingsPage() {
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     const res = await axios
       .post(`${URL_USER_SVC}/logout`, {}, { withCredentials: true })
       .catch((err) => {
         if (err.response.status === STATUS_CODE_INTERNAL_SERVER_ERROR) {
-          setErrorDialog('Unable to log out');
+          console.log('Unable to log out');
         } else {
-          setErrorDialog('Please try again later');
+          console.log('Please try again later');
         }
       });
     if (res && res.status === STATUS_CODE_OK) {
-      handleNavigation();
+      navigate('../login');
       logout();
     }
-  };
-
-  const handleDelete = async () => {
-    const res = await axios.delete(`${URL_USER_SVC}/`, { withCredentials: true }).catch((err) => {
-      if (err.response.status === STATUS_CODE_INTERNAL_SERVER_ERROR) {
-        setErrorDialog('Unable to delete');
-      } else if (err.response.status === STATUS_CODE_UNAUTHORISED) {
-        setErrorDialog('Not Authenticated. Please log in again.');
-      } else if (err.response.status === STATUS_CODE_FORBIDDEN) {
-        setErrorDialog('Unauthorised Action. Please log in again.');
-      } else {
-        setErrorDialog('Please try again later');
-      }
-    });
-    if (res && res.status === STATUS_CODE_OK) {
-      handleNavigation();
-      logout();
-    }
-  };
-
-  const handleChangePassword = async () => {
-    const res = await axios
-      .patch(
-        `${URL_USER_SVC}/change-password`,
-        { password: newPassword },
-        { withCredentials: true }
-      )
-      .catch((err) => {
-        if (err.response.status === STATUS_CODE_INTERNAL_SERVER_ERROR) {
-          setErrorDialog('Unable to change password');
-        } else if (err.response.status === STATUS_CODE_UNAUTHORISED) {
-          setErrorDialog('Not Authenticated. Please log in again.');
-        } else if (err.response.status === STATUS_CODE_FORBIDDEN) {
-          setErrorDialog('Unauthorised Action. Please log in again.');
-        } else {
-          setErrorDialog('Please try again later');
-        }
-      });
-    if (res && res.status === STATUS_CODE_OK) {
-      setSuccessDialog('Password changed successfully');
-    }
-  };
-
-  const closeDialog = () => {
-    setIsDialogOpen(false);
-    setDialogButtonMessage('');
-    setDialogTitle('');
-  };
-
-  const setErrorDialog = (msg: any) => {
-    setIsDialogOpen(true);
-    setDialogTitle('Error');
-    setDialogMsg(msg);
-  };
-
-  const setSuccessDialog = (msg: any) => {
-    setIsDialogOpen(true);
-    setDialogTitle('Success');
-    setDialogMsg(msg);
-  };
-
-  const setChangePasswordConfirmationDialog = (msg: string) => {
-    setIsDialogOpen(true);
-    setButtonMode('changePassword');
-    setIsDialogInputEnabled(true);
-    setIsDialogButtonEnabled(true);
-    setDialogButtonMessage(msg);
-    setDialogTitle(msg);
-  };
-
-  const setDeleteConfirmationDialog = (msg: any) => {
-    setIsDialogOpen(true);
-    setButtonMode('deleteAccount');
-    setIsDialogInputEnabled(false);
-    setIsDialogButtonEnabled(true);
-    setDialogTitle('Delete Account');
-    setDialogButtonMessage(msg);
-  };
-
-  const handleNavigation = () => {
-    navigate('../login');
   };
 
   return (
     <Box
-      display={'flex'}
-      flexDirection={'column'}
-      width={'90%'}
-      alignItems={'center'}
-      fontFamily={'Arimo'}
+      className="Settings"
+      width="90%"
+      height="90%"
+      bgcolor={'primary.main'}
+      gridRow={2}
       borderRadius={'10px'}
       padding={'5%'}
-      style={{ backgroundColor: 'white' }}
-    >
+      display={'flex'}>
+      <Box
+        display={'flex'}
+        width={'50%'}
+        className="Logo"
+        justifyContent={'center'}
+        alignItems={'center'}>
+        <Logo />
+      </Box>
       <Box
         display={'flex'}
         flexDirection={'column'}
-        justifyContent={'flex-end'}
+        justifyContent={'center'}
         alignItems={'center'}
-      >
+        width={'50%'}>
         <Typography variant={'h3'} marginBottom={'2rem'} fontFamily={'Arimo'}>
           Settings
         </Typography>
         <Button
           variant={'outlined'}
-          onClick={() => {
-            setChangePasswordConfirmationDialog('Change Password');
-            setSuccessDialog('');
-          }}
-          style={{
-            color: 'white',
-            borderColor: 'white',
-            background: 'linear-gradient(90deg, #AC44B0, #EF429A)'
-          }}
-        >
+          color={'primary'}
+          sx={{
+            background: 'linear-gradient(90deg, #AC44B0, #EF429A)',
+            margin: '0.5rem',
+            width: '65%'
+          }}>
           Change Password
         </Button>
         <Button
           variant={'outlined'}
+          color={'primary'}
           onClick={handleLogout}
-          style={{
-            color: 'white',
-            borderColor: 'white',
-            background: 'linear-gradient(90deg, #AC44B0, #EF429A)'
-          }}
-        >
+          sx={{
+            background: 'linear-gradient(90deg, #AC44B0, #EF429A)',
+            margin: '0.5rem',
+            width: '65%'
+          }}>
           Log Out
         </Button>
         <Button
           variant={'outlined'}
-          onClick={() => setDeleteConfirmationDialog('Delete Account')}
-          style={{
-            color: 'white',
-            borderColor: 'white',
-            background: 'linear-gradient(90deg, #AC44B0, #EF429A)'
-          }}
-        >
+          color={'primary'}
+          sx={{
+            background: 'linear-gradient(90deg, #AC44B0, #EF429A)',
+            margin: '0.5rem',
+            width: '65%'
+          }}>
           Delete Account
         </Button>
       </Box>
-
-      <Dialog open={isDialogOpen} onClose={closeDialog}>
-        <DialogTitle>{dialogTitle}</DialogTitle>
-        <DialogContent>
-          <DialogContentText>{dialogMsg}</DialogContentText>
-          {isDialogButtonEnabled ? (
-            <Box>
-              {isDialogInputEnabled ? (
-                <TextField
-                  label="New Password"
-                  variant="standard"
-                  type="password"
-                  value={newPassword}
-                  onChange={(e) => {
-                    setNewPasswordErrorState(false);
-                    setNewPassword(e.target.value);
-                  }}
-                  sx={{ marginBottom: '1rem', width: '75%' }}
-                  autoFocus
-                  required
-                  error={newPasswordErrorState}
-                />
-              ) : null}
-              <Button
-                onClick={
-                  buttonMode === 'changePassword'
-                    ? () => {
-                        if (newPassword === '') {
-                          setNewPasswordErrorState(true);
-                          return;
-                        }
-                        handleChangePassword();
-                        setIsDialogButtonEnabled(false);
-                        setButtonMode('');
-                        closeDialog();
-                      }
-                    : () => {
-                        handleDelete();
-                        setIsDialogButtonEnabled(false);
-                        setButtonMode('');
-                        closeDialog();
-                      }
-                }
-              >
-                {dialogButtonMessage}
-              </Button>
-            </Box>
-          ) : null}
-        </DialogContent>
-      </Dialog>
     </Box>
   );
 }
-
-export default SettingsPage;
