4688f53be2f226e3f21f72f17f429c4820c3011c
Jonathan-Cao <54720093+Jonathan-Cao@users.noreply.github.com>
2022-11-06 17:41:01 +0800

    add deployment files

--- apps/frontend/src/components/MatchPage.tsx
+++ apps/frontend/src/components/MatchPage.tsx
@@ -17,8 +17,9 @@ import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 import { useNavigate } from 'react-router-dom';
 import { useMatch } from '../context/MatchContext';
 import { Logo } from './Logo';
+import { URL_MATCHING_SVC } from '../configs';
 
-const socket = io('http://localhost:8001');
+const socket = io(`${URL_MATCHING_SVC}`);
 
 const DIFFICULTY = {
   EASY: 'Easy',
@@ -71,15 +72,13 @@ function DifficultySelectPage() {
       gridRow={2}
       borderRadius={'10px'}
       padding={'5%'}
-      display={'flex'}
-    >
+      display={'flex'}>
       <Box
         display={'flex'}
         width={'50%'}
         className="Logo"
         justifyContent={'center'}
-        alignItems={'center'}
-      >
+        alignItems={'center'}>
         <Logo />
       </Box>
       <Box
@@ -87,8 +86,7 @@ function DifficultySelectPage() {
         flexDirection={'column'}
         justifyContent={'center'}
         alignItems={'center'}
-        width={'50%'}
-      >
+        width={'50%'}>
         <Typography variant={'h3'} marginBottom={'2rem'} width={'75%'} textAlign={'center'}>
           Select Difficulty
         </Typography>
@@ -97,8 +95,7 @@ function DifficultySelectPage() {
             defaultValue={DIFFICULTY.EASY}
             value={difficulty}
             name="radio-buttons-group"
-            onChange={(e) => setDifficulty(e.target.value)}
-          >
+            onChange={(e) => setDifficulty(e.target.value)}>
             <FormControlLabel
               value={DIFFICULTY.EASY}
               control={<Radio />}
@@ -127,8 +124,7 @@ function DifficultySelectPage() {
             borderColor: 'white',
             background: 'linear-gradient(90deg, #AC44B0, #EF429A)',
             width: '75%'
-          }}
-        >
+          }}>
           Match
         </Button>
 
@@ -140,8 +136,7 @@ function DifficultySelectPage() {
               duration={30}
               colors={['#004777', '#F7B801', '#A30000', '#A30000']}
               colorsTime={[25, 15, 5, 0]}
-              onComplete={onNoMatchFound}
-            >
+              onComplete={onNoMatchFound}>
               {({ remainingTime }) => remainingTime}
             </CountdownCircleTimer>
           </DialogContent>
