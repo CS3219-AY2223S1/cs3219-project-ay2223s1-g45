1013aa4468b2d2fb974e56853860a37956bdf071
Aditya Banerjee <aditya_banerjee@icloud.com>
2022-11-09 15:19:38 +0800

    update README.md

--- apps/question-service/controllers/questionController.ts
+++ apps/question-service/controllers/questionController.ts
@@ -1,4 +1,22 @@
+/* eslint-disable no-bitwise */
 import Question from '../model/question';
+
+/**
+ * Returns a hash code from a string
+ * @param  {String} str The string to hash.
+ * @return {Number}    A 32bit integer
+ * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+ */
+function hashCode(str: String) {
+  let hash = 0;
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0, len = str.length; i < len; i++) {
+    const chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
 
 export const get = (req: any, res: any) => {
   Question.find((err, questions) => {
@@ -40,19 +58,3 @@ export const getById = (req: any, res: any) => {
     });
   });
 };
-
-/**
- * Returns a hash code from a string
- * @param  {String} str The string to hash.
- * @return {Number}    A 32bit integer
- * @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
- */
-function hashCode(str: String) {
-  let hash = 0;
-  for (let i = 0, len = str.length; i < len; i++) {
-    let chr = str.charCodeAt(i);
-    hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-}
